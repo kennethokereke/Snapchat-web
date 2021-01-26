a528d9d22abe360af47aca5492f9d35ddfcfad0f
Kenneth Okereke <kenneth.okereke@wagner.edu>
2021-01-26 12:31:39 -0500

    new changes

--- src/Components/PostChat.js
+++ src/Components/PostChat.js
@@ -1,8 +1,20 @@
+import { Avatar } from '@material-ui/core'
+import StopRoundedIcon from '@material-ui/icons/StopRounded'
 import React from 'react'
+import ReactTimeago from 'react-timeago'
+import '../Stylesheet/PostChat.css'
 
-function PostChat() {
+function PostChat({id, username, timestamp, read, imageUrl, profilePic}) {
     return (
-        <div>
+        <div className="chat">
+       <Avatar className="chat__avatar" src={profilePic}/>
+       <div className="chat__info">
+           <h4>{username}</h4>
+           <p style={{fontSize: 12}}>Tap to view - {" "} <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()}/></p>
+
+
+       </div>
+       {!read && <StopRoundedIcon className="chats__readIcon"/>}
             
         </div>
     )
